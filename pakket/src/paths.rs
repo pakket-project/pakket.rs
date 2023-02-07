3368d6ac93dbf6d935415f06e4dccfda07604490
Kees van Voorthuizen <me@keesvv.nl>
2023-02-07 22:45:30 +0100

    fix(cli): update lockfile path & fix hardcoded name

--- pakket/src/paths.rs
+++ pakket/src/paths.rs
@@ -8,12 +8,12 @@ pub enum Path {
 
 impl Path {
     fn get_project_dirs(&self) -> ProjectDirs {
-        ProjectDirs::from("", "", "pakket").unwrap()
+        ProjectDirs::from("", "", env!("CARGO_PKG_NAME")).unwrap()
     }
 
     pub fn to_path_buf(&self) -> PathBuf {
         match self {
-            Self::Lockfile => self.get_project_dirs().config_dir().into(),
+            Self::Lockfile => self.get_project_dirs().config_dir().join("lock.toml"),
         }
     }
 }
